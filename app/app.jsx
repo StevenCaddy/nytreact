024412af761e05888760a0dc6a6c0e5173ebc93f
Steven Cadwallader <steven.caddie@gmail.com>
2017-12-09 07:43:39 -0600

    github

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+// Include the Main React Dependencies
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Parent Component
+var Main = require("./components/children/Main.jsx");
+
+ReactDOM.render(<Main />, document.getElementById("app"));
